aa1388a506d75c0aebcbd67550e83bd6a0973854
awasthypranshu <149893584+awasthypranshu@users.noreply.github.com>
2026-07-26 22:03:36 +0530

    Create 2058-ConcatenationOfArray

---
+++ https:/github.com/awasthypranshu/Leetcode_Tracker.git/2058-ConcatenationOfArray/2058-ConcatenationOfArray.cpp
@@ -0,0 +1,12 @@
+// Last updated: 26/07/2026, 22:03:36
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int>ans;
+        ans = nums;
+        for(int i = 0; i < nums.size(); i++){
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
