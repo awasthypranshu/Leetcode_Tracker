6db074bc7fc543f2a9f73ad21ea3bced05886943
awasthypranshu <149893584+awasthypranshu@users.noreply.github.com>
2026-07-26 22:05:02 +0530

    Create 344-ReverseString

---
+++ https:/github.com/awasthypranshu/Leetcode_Tracker.git/344-ReverseString/344-ReverseString.cpp
@@ -0,0 +1,14 @@
+// Last updated: 26/07/2026, 22:05:02
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            st.push(s[i]);
+        }
+        for(int i = 0; i < s.size(); i++){
+            s[i] = st.top();
+            st.pop();
+        }
+    }
+};
