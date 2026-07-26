a09d9bdbe66e0aac9b621c4656eaca81786145a8
awasthypranshu <149893584+awasthypranshu@users.noreply.github.com>
2026-07-26 22:05:47 +0530

    Create 88-MergeSortedArray

---
+++ https:/github.com/awasthypranshu/Leetcode_Tracker.git/88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -0,0 +1,11 @@
+// Last updated: 26/07/2026, 22:05:47
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        for(int i = 0; i < n; i++){
+            nums1[m+i] = nums2[i];  
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
