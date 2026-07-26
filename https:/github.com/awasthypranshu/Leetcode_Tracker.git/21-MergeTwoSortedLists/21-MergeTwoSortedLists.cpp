7ae4b3b1bb1c5a7280ddd7786a19c2dc4e37227c
awasthypranshu <149893584+awasthypranshu@users.noreply.github.com>
2026-07-26 22:06:13 +0530

    Create 21-MergeTwoSortedLists

---
+++ https:/github.com/awasthypranshu/Leetcode_Tracker.git/21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -0,0 +1,46 @@
+// Last updated: 26/07/2026, 22:06:13
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+
+        ListNode* newHead;
+        if(list1->val <= list2->val){
+            newHead = list1;
+            list1 = list1->next;
+        }else{
+            newHead = list2;
+            list2 = list2->next;
+        }
+
+        ListNode* temp = newHead;
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val <= list2->val){
+                temp->next = list1;
+                list1 = list1->next;
+            }else{
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+        if(list1 != NULL){
+            temp->next = list1;
+        }else{
+            temp->next = list2;
+        }
+
+        return newHead;
+    }
+};
